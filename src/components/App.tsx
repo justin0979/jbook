1b21ac5a119d68c473c4e81d9db1d2d257699956
justin0979 <justinm0979@gmail.com>
2021-06-20 17:31:11 -0500

    add textarea and button that will put input into state

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,24 @@
-import ripple from "&src/ripple.svg";
+import { useState } from "react";
 
 const App: React.FC = () => {
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+
+  const onClick = () => {
+    setInput("");
+    console.log(input);
+  };
+
   return (
     <div className="app" data-test="appComponent">
-      <h1>hi</h1>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
     </div>
   );
 };
