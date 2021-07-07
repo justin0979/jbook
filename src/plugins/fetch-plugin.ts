64834f89d943b42f5b71e8d7f7cee6a3f03ebea5
justin0979 <justinm0979@gmail.com>
2021-07-07 16:00:08 -0500

    refactor duplicate caching code to separate onLoad function

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -20,7 +20,7 @@ export const fetchPlugin = (inputCode: string) => {
         },
       );
 
-      build.onLoad({ filter: /.css$/ }, async (args: any) => {
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
         //  Check to see if we have already fetched this file
         //  and if it is in the cache
         const cachedResult =
@@ -32,7 +32,9 @@ export const fetchPlugin = (inputCode: string) => {
         if (cachedResult) {
           return cachedResult;
         }
+      });
 
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
         const { data, request } = await axios.get(args.path);
 
         const escaped = data
@@ -59,18 +61,6 @@ export const fetchPlugin = (inputCode: string) => {
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        // Check to see if we have already fetched this file
-        // and if it is in the cache
-        const cachedResult =
-          await fileCache.getItem<esbuild.OnLoadResult>(
-            args.path,
-          );
-
-        // if it is, return it immediately
-        if (cachedResult) {
-          return cachedResult;
-        }
-
         const { data, request } = await axios.get(args.path);
 
         const result: esbuild.OnLoadResult = {
