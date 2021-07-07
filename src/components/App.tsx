ab2c995e7576a641700ec1c338521a8671ec1c9a
justin0979 <justinm0979@gmail.com>
2021-07-07 16:11:44 -0500

    refactor retrieval of esbuild.wasm to use unpkg instead of adding .wasm file to public dir.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -15,7 +15,8 @@ const App: React.FC = () => {
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: "/esbuild.wasm",
+      wasmURL:
+        "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
     });
   };
 
