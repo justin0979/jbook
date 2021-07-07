21ee4cee80d9c1196fd27579b662ef2cddc868af
justin0979 <justinm0979@gmail.com>
2021-07-07 15:00:04 -0500

    extract onLoad to separate fetch-plugin.ts file

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import * as esbuild from "esbuild-wasm";
 import { unpkgPathPlugin } from "&plugins/unpkg-path-plugin";
+import { fetchPlugin } from "&plugins/fetch-plugin";
 
 const App: React.FC = () => {
   const [input, setInput] = useState("");
@@ -27,7 +28,7 @@ const App: React.FC = () => {
       entryPoints: ["index.js"],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin(input)],
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
       define: {
         "process.env.NODE_ENV": "'production'",
         global: "window",
