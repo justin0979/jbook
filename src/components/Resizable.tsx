2c7f2df9a1130abb6d79d27446bc5ce21267f716
justin0979 <justinm0979@gmail.com>
2021-08-04 10:12:44 -0500

    use debounce to help prevent lagginess on resize

--- src/components/Resizable.tsx
+++ src/components/Resizable.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import {
   ResizableBox,
   ResizableBoxProps,
@@ -13,19 +13,30 @@ export const Resizable: React.FC<ResizableProps> = ({
   children,
 }) => {
   let resizableProps: ResizableBoxProps;
-  const [innerHeight, setInnerHeight] = useState(window.innerHeight)
-  const [innerWidth, setInnerWidth] = useState(window.innerWidth)
+  const [innerHeight, setInnerHeight] = useState(
+    window.innerHeight,
+  );
+  const [innerWidth, setInnerWidth] = useState(
+    window.innerWidth,
+  );
 
   useEffect(() => {
-      const listener = () => {
-        setInnerWidth(window.innerWidth)
-        setInnerHeight(window.innerHeight)
+    let timer: any;
+    const listener = () => {
+      if (timer) {
+        clearTimeout(timer);
       }
+      timer = setTimeout(() => {
+        setInnerWidth(window.innerWidth);
+        setInnerHeight(window.innerHeight);
+      }, 100);
+    };
 
-      window.addEventListener("resize", listener)
+    window.addEventListener("resize", listener);
 
-      return () => window.removeEventListener("resize", listener)
-      }, [])
+    return () =>
+      window.removeEventListener("resize", listener);
+  }, []);
 
   if (direction === "horizontal") {
     resizableProps = {
