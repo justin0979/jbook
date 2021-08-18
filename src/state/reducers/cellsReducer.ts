4c6c9b3a0888b31a86109e44000687f0071ca2ab
justin0979 <justinm0979@gmail.com>
2021-08-18 12:24:37 -0500

    handle moving cells up or down

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -34,7 +34,24 @@ const reducer = produce(
         );
         return;
       case ActionType.MOVE_CELL:
-        return state;
+        const { direction } = action.payload;
+        const index = state.order.findIndex(
+          (id) => id === action.payload.id,
+        );
+        const targetIndex =
+          direction === 'up' ? index - 1 : index + 1;
+
+        if (
+          targetIndex < 0 ||
+          targetIndex > state.order.length - 1
+        ) {
+          return;
+        }
+
+        state.order[index] = state.order[targetIndex];
+        state.order[targetIndex] = action.payload.id;
+
+        return;
       case ActionType.INSERT_CELL_BEFORE:
         return state;
       default:
