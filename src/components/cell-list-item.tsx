c097dad76294d2e446735feea247bdafdf225880
justin0979 <justinm0979@gmail.com>
2021-08-30 17:16:46 -0500

    add initial CellListItem and CellList components

---
+++ src/components/cell-list-item.tsx
@@ -0,0 +1,9 @@
+const CellListItem: React.FC = () => {
+  return (
+    <div>
+      <h1>Cell List Item</h1>
+    </div>
+  );
+};
+
+export default CellListItem;
---
+++ src/components/cell-list.tsx
@@ -0,0 +1,9 @@
+const CellList: React.FC = () => {
+  return (
+    <div>
+      <h1>Cell List</h1>
+    </div>
+  );
+};
+
+export default CellList;
