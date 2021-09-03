42082c4a83df9440b872cb7bcd2d84d4e83b33dc
justin0979 <justinm0979@gmail.com>
2021-09-03 08:55:06 -0500

    add useActions hook

--- src/state/store.ts
+++ src/state/store.ts
@@ -26,5 +26,21 @@ store.dispatch({
   },
 });
 
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: 'code',
+  },
+});
+
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: 'text',
+  },
+});
+
 // const state = store.getState();
 // console.log(state);
