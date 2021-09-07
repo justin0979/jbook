7512624c91f09cbd0eac6f5d0e39b54cdf75fffa
justin0979 <justinm0979@gmail.com>
2021-09-07 11:13:20 -0500

    refactor CellListItem to have ActionBar in each if statement

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,8 +1,6 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { merge } = require('webpack-merge');
 const commonConfig = require('./webpack.common');
-//const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(commonConfig, {
   entry: ['./src/index'],
