ab0bf6406d36c93d755cc986b79e4247f48d135f
justin0979 <justinm0979@gmail.com>
2021-09-15 18:28:31 -0500

    add progress bar in Preview

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -45,7 +45,16 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
             onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
-        {bundle && (
+        {!bundle || bundle.loading ? (
+          <div className="progress-cover">
+            <progress
+              className="progress is-small is-primary"
+              max="100"
+            >
+              Loading
+            </progress>
+          </div>
+        ) : (
           <Preview code={bundle.code} err={bundle.err} />
         )}
       </div>
